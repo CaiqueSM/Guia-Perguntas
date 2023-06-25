f0bd3ffb6d38cbe469e7585d9ca85baf8fc8464c
caimachado <caique9machado@gmail.com>
2023-06-25 19:27:31 -0300

    Adicionando bootstrap a página

--- index.js
+++ index.js
@@ -4,28 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:nome/:lang', (req, res) => {
-    //res.send('Bem vindo ao meu site!');
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "biscoito", preco: 3.14},
-        {nome: "refrigerante", preco: 5.0},
-        {nome: "leite", preco: 1.15},
-        {nome: "Carne", preco: 32.90},
-        {nome: "energético", preco: 9.15},
-    ];
-
-    res.render('index', {
-        nome,
-        lang,
-        produtos,
-        empresa: 'Guia perguntas',
-        inscritos: 8,
-        msg: exibirMsg
-    });
+app.get("/", (req, res) =>{
+    res.render("index");
 });
 
 app.listen(8080, () =>{
