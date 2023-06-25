48c80ec4228bbbef4e278c9ced3e73a80468b2e2
caimachado <caique9machado@gmail.com>
2023-06-25 18:22:31 -0300

    Criando primeira rota.

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/:nome/:lang', (req, res) => {
     //res.send('Bem vindo ao meu site!');
-    res.render('index');
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome: "biscoito", preco: 3.14},
+        {nome: "refrigerante", preco: 5.0},
+        {nome: "leite", preco: 1.15},
+        {nome: "Carne", preco: 32.90},
+        {nome: "energético", preco: 9.15},
+    ];
+
+    res.render('index', {
+        nome,
+        lang,
+        produtos,
+        empresa: 'Guia perguntas',
+        inscritos: 8,
+        msg: exibirMsg
+    });
 });
 
 app.listen(8080, () =>{
